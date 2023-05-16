1b7de874981884a178fe9380fc8881c33349e960
shichongwei <shichongwei@datagrand.com>
2023-05-16 11:51:31 +0800

    cs

--- go-demo/main.go
+++ go-demo/main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-	utils_ip "go-demo/utils/ip"
-)
-
 func main() {
-	ip := utils_ip.GetInternetIP()
-	fmt.Println(ip)
-	fmt.Println(utils_ip.GetLocalIP())
+	adaptor.
 }
