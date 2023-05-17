2c9844a4b8d68cfe919b774b717f40e4ec6a68f6
shichongwei <shichongwei@datagrand.com>
2023-05-17 11:27:28 +0800

    cs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module one_index
 
 go 1.20
+
+require github.com/tidwall/match v1.1.1
--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	l1 := []int{1, 2}
-	fmt.Println(l1)
-	l1 = append(l1, 3)
-	fmt.Println(l1)
+	chs := [3]chan int{
+		make(chan int, 1),
+		make(chan int, 1),
+		make(chan int, 1),
+	}
+
+	index := rand.Intn(3) // 随机生成0-2之间的数字
+	fmt.Printf("随机索引/数值: %d\n", index)
+	chs[index] <- index // 向通道发送随机数字
+
+	// 哪一个通道中有值，哪个对应的分支就会被执行
+	select {
+	case <-chs[0]:
+		fmt.Println("第一个条件分支被选中")
+	case <-chs[1]:
+		fmt.Println("第二个条件分支被选中")
+	case num := <-chs[2]:
+		fmt.Println("第三个条件分支被选中:", num)
+	default:
+		fmt.Println("没有分支被选中")
+	}
 }
