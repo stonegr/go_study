cd210e31c5d45829db41784082b627013e137f61
shichongwei <shichongwei@datagrand.com>
2023-06-28 20:29:35 +0800

    基础学习

--- suiji/main.go
+++ suiji/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"suiji/my_moudle"
-)
-
 func main() {
 	// fmt.Println("指针处理开始")
 	// my_moudle.Do_zz()
@@ -15,7 +10,20 @@ func main() {
 	// fmt.Println("数组处理开始")
 	// my_moudle.Do_List()
 	// fmt.Println("数组处理结束")
-	fmt.Println("结构处理开始")
-	my_moudle.Do_List()
-	fmt.Println("结构处理结束")
+	// fmt.Println("结构处理开始")
+	// my_moudle.Do_d()
+	// fmt.Println("结构处理结束")
+	// fmt.Println("map处理开始")
+	// my_moudle.Do_map()
+	// fmt.Println("map处理结束")
+	// fmt.Println("interface 处理开始")
+	// my_moudle.Do_jk()
+	// fmt.Println("interface 处理结束")
+	// fmt.Println("go并发 处理开始")
+	// my_moudle.Do_go()
+	// fmt.Println("go并发 处理结束")
+
+	// 模块学习
+	// redis 操作
+
 }
