a0184fd4fe5dd2939563b81802fe8d80c94dd10f
stonegr <mengxuanmr@gmail.com>
2023-09-24 15:20:16 +0800

    cs

--- m3/tests/cs.go
+++ m3/tests/cs.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"net/url"
 )
 
 func main() {
-	a := "123ab='234'"
-	r := strings.Index(a, "ab")
-	quotation_mark_pos := strings.LastIndex(a, "'")
-	fmt.Println(r, quotation_mark_pos)
-	fmt.Println(a[r:quotation_mark_pos])
+	// a := "123ab='234'"
+	// r := strings.Index(a, "ab")
+	// quotation_mark_pos := strings.LastIndex(a, "'")
+	// fmt.Println(r, quotation_mark_pos)
+	// fmt.Println(a[r:quotation_mark_pos])
+
+	a := "https://baidu.com/aa"
+	// r, _ := url.JoinPath(a, "wer")
+	// r, _ := url.JoinPath(a, "/wer")
+
+	r, _ := url.JoinPath(a, "https://baidu.com")
+	fmt.Println(r)
 }
